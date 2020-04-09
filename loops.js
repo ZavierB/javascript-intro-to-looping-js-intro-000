27e49c5c87d77ba73a224bafc3250564be9e73a3
Zavier Berolo <zavier.berolo@gmail.com>
2020-04-09 10:28:07 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -4,10 +4,10 @@ let array = [];
 function forLoop(array) {
   for (let i = 0; i < 25; i++) {
     if(i === 1){
-      array.concat('I am 1 strange loop.')
+      array.pop('I am 1 strange loop.')
     }
     else {
-      array.concat(`I am ${i} strange loops.`)
+      array.push(`I am ${i} strange loops.`)
     }
   }
   return array
