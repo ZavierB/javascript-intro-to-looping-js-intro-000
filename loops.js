c988eea0419032c3e072103aed17160583bbf5b3
Zavier Berolo <zavier.berolo@gmail.com>
2020-04-09 12:04:16 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -25,19 +25,19 @@ function whileLoop(n) {
 
 function doWhileLoop(num){
 do{
-  
+
 }
 
 
 } while
 
 (let i = 0;
- 
+
 function incrementVariable() {
   i = i + 1;
   return i;
 }
- 
+
 do {
   console.log("I run once regardless.");
 } while (incrementVariable() num);
