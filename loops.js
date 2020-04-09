bc73bebb3a036479d166ab3abb941bba0be4f514
Zavier Berolo <zavier.berolo@gmail.com>
2020-04-09 11:59:02 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -21,3 +21,16 @@ function whileLoop(n) {
   }
   return "done"
 }
+
+
+function doWhileLoop(num){
+let i = 0;
+
+  function incrementVariable() {
+    i = i + 1;
+      return i;
+}
+  do {
+  console.log("doo-bee-doo-bee-doo");
+  } while (incrementVariable() < 5);
+}
